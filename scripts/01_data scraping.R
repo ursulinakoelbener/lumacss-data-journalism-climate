2e415d107b2e2761931ad67931201ac0f17e99cd
Ursulina Kölbener <ursulina@koelbener.ch>
2025-06-02 19:31:28 +0200

    delete script

--- scripts/01_data scraping.R
+++
@@ -1,9 +0,0 @@
-# relevant packages
-library(tidyverse)
-library(rvest)
-library(purrr)
-library(dplyr)
-library(janitor)
-
-# list of climate strikes in Switzerland
-url <- "https://climatestrike.ch/events"
