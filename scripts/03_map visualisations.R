b0a95ae9deec9faca0f7f6644f9611c188d4126c
Ursulina Kölbener <ursulina@koelbener.ch>
2025-06-02 21:22:15 +0200

    update code

--- scripts/03_map visualisations.R
+++ scripts/03_map visualisations.R
@@ -7,6 +7,7 @@ library(gganimate)
 library(transformr)
 library(rnaturalearth)
 library(rnaturalearthdata)
+library(lubridate)
 library(gifski)  # Für das Rendern als GIF
 
 swiss_data <- read_csv("data/Enriched Swiss protest data.csv")
@@ -18,7 +19,11 @@ switzerland <- world %>% filter(admin == "Switzerland")
 
 # 2. filter data ----
 climate_data <- swiss_data %>%
-  filter(Topic == "Climate")
+  filter(Topic == "Climate")  %>%
+  mutate(
+    event_date = as.Date(event_date),
+    event_month = floor_date(event_date, "month")  # z.B. 2025-05-01
+  )
 
 # 3. create sf-object ----
 climate_sf <- st_as_sf(
@@ -31,18 +36,18 @@ climate_sf <- st_as_sf(
 # basic plot
 p <- ggplot() +
   geom_sf(data = switzerland, fill = "white", color = "black") +
-  geom_sf(data = climate_sf, color = "red", size = 3) +
-  labs(title = "Klimaproteste in der Schweiz: {frame_time}") +
+  geom_sf(data = climate_sf, aes(color = "red", size = final_estimate)) +
+  scale_size(range = c(4, 14)) +
+  labs(title = "Klimaproteste in der Schweiz: {format(frame_time, '%B %Y')}") +
   theme_minimal()
 
 #  Animation definieren
 animation <- p +
-  transition_time(event_date) +
-  shadow_mark(past = TRUE, alpha = 0.2) +
+  transition_time(climate_sf$event_month) +
   ease_aes("linear")
 
 # GIF exportieren
 animate(animation,
         width = 700, height = 700,
-        duration = 10, fps = 10,
+        duration = 20, fps = 5,
         renderer = gifski_renderer("plot/climate_protests.gif"))
